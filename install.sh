d0daa6729dca93cf7a74a509a5003e0743cf1b58
tran <tran@trans-iMac-Pro.local>
2022-10-06 21:42:42 +0700

    final

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@
 #Enjoy!
 #P/S: This pack contains: Visual Studio Code (AUR); SimpleScreenRecorder(PM); Google Chrome (AUR); VMware (Bundle file)
 #-----------------------------------------------------------------------------------------------------------------------------------------------
+# Some colors :D
 sudo pacman -S figlet lolcat git #Init and install component
 figlet -c Tran1903  #Self-ads
 figlet -c was here  # Self-ads
